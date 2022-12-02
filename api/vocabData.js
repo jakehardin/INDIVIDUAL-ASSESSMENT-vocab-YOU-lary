153aa7b3411ff5a083f7eae451ee3406ed6e3cfc
jakehardin <jakehardin13@gmail.com>
2022-12-01 18:04:06 -0600

    got the vocabulary words on the dom and added more files

--- api/vocabData.js
+++ api/vocabData.js
@@ -3,7 +3,7 @@ import client from '../utils/client';
 
 const endpoint = client.databaseURL;
 
-const getVocabulary = () => new Promise((resolve, reject) => {
+const getVocab = () => new Promise((resolve, reject) => {
   fetch(`${endpoint}/vocabulary.json`, {
     method: 'GET',
     headers: {
@@ -15,4 +15,17 @@ const getVocabulary = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-export default getVocabulary;
+// TODO: DELETE BOOK
+const deleteVocab = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export { getVocab, deleteVocab };
---
+++ utils/startApp.js
@@ -0,0 +1,22 @@
+import { getVocab } from '../api/vocabData';
+import logoutButton from '../components/logoutButton';
+import domBuilder from '../components/shared/domBuilder';
+import navBar from '../components/shared/navBar';
+import domEvents from '../events/domEvents';
+// import formEvents from '../events/formEvents';
+import navigationEvents from '../events/navigationEvents';
+import { showVocab } from '../pages/vocabulary';
+
+const startApp = () => {
+  domBuilder(); // BUILD THE DOM
+  domEvents(); // ADD THE EVENT LISTENTERS TO THE DOM
+  // formEvents(); // ADD FORM EVENT LISTENTERS TO THE DOM
+  navBar(); // DYNAMICALLY ADD THE NAV
+  logoutButton(); // ADD THE LOGOUT BUTTON COMPONENT
+  navigationEvents(); // ATTACH THE EVENT LISTENERS TO THE NAVBAR
+
+  // Put all books on the DOM on App load
+  getVocab().then((vocabulary) => showVocab(vocabulary));
+};
+
+export default startApp;
